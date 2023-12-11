052a09afda808a7be1cc93d6180a788dd01b6828
localadmin <agatha.mackowska@gmail.com>
2023-12-11 12:50:08 +0100

    change

--- 05_Petle/while.py
+++ 05_Petle/while.py
@@ -4,6 +4,15 @@ while n > 0:
     print(n)
     n -= 1
 
+
+
+for a in range(1, 100):
+    b = a % 10
+    c = a // 10
+    i = b + c
+    if (i % 7 == 0 and i % 2 == 0):
+        print(f"{a} liczba jest dobra")
+
 n = 8
 while n > 0:
     n -= 1
@@ -37,7 +46,7 @@ while n > 0:
 else:
     print('Koniec')
 
-n = 8
+n = 15
 while n > 0:
     n -= 1
     if n % 2 == 0:
